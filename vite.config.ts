1f6f5a09894e5e259fce7ab6e50545594705bb5d
ibraum <ibraumodnok@gmail.com>
2025-04-06 14:17:13 +0000

    fix: fileURLToPath build

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import tailwindcss from '@tailwindcss/vite';
-import {dirname, resolve} from 'path';
-import fileURLToPath from "url";
+import { dirname, resolve } from 'path';
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
