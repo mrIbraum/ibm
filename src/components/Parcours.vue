2fa45772da9d38975119e528cf6b591634017497
ibraum <ibraumodnok@gmail.com>
2025-04-09 09:19:01 +0000

    services

--- src/components/Parcours.vue
+++ src/components/Parcours.vue
@@ -1,11 +1,39 @@
 <script setup lang="ts">
 
+    const parcours = [
+        {
+            id: 1,
+            logo: "",
+            title: "Architecture Logiciel",
+            year: "2022 - present"
+        },
+        {
+            id: 1,
+            logo: "",
+            title: "Stage En Support Services Vas.",
+            year: "2022 - present"
+        },
+        {
+            id: 1,
+            logo: "",
+            title: "Student On DataCamp Plateform.",
+            year: "2022 - present"
+        }
+    ];
 </script>
 
 <template>
     <div class="w-full min-h-[100vh] flex items-center justify-between relative overflow-hidden">
-        <div class="w-full min-h-[100vh] pt-[25px] px-[75px] flex flex-col items-center justify-start">
+        <img src="../assets/img/manifesto-extra.tracks.png" alt="" class="absolute w-[500px] left-[-100px] opacity-[0.75]">
+        <img src="../assets/img/manifesto-extra.tracks.png" alt="" class="absolute w-[500px] bottom-[-200px] right-[-100px] opacity-[0.5] rotate-[180deg]">
+        <div class="w-full min-h-[100vh] pt-[25px] px-[75px] gap-7 flex flex-col items-center justify-start">
             <div class="bg-[linear-gradient(90deg,var(--color),white)] bg-clip-text text-transparent text-[2.5vw] font-bold z-1">Parcours</div>
+            <div class="w-[80%] min-h-[300px] text-white text-center z-1">
+                <div v-for="parcour in parcours" class="flex items-center justify-start gap-6 w-full px-5 py-[4rem] bg-stone-100 mb-6 rounded relative bg-[linear-gradient(45deg,#1D1D1D,#090909,#1D1D1D,#090909,#1D1D1D)] hover:bg-[linear-gradient(45deg,#242424,#090909,#1D1D1D)] duration-300">
+                    <div class="text-3xl font-bold text-slate-500">{{ parcour.title }}</div>
+                    <div class="w-full h-full bg-[linear-gradient(45deg,var(--color))] opacity-[0.1] absolute top-0 left-0 z-0"></div>
+                </div>
+            </div>
         </div>
         <div class="h-[200px] w-full bg-[linear-gradient(180deg,#000000,rgba(0,0,0,0))] absolute top-0 z-0"></div>
         <div class="h-[100px] w-full bg-[linear-gradient(0deg,#000000,rgba(0,0,0,0))] absolute bottom-0 z-0"></div>
--- src/components/Services.vue
+++ src/components/Services.vue
@@ -6,6 +6,7 @@
     <div class="w-full min-h-[100vh] flex items-center justify-between relative overflow-hidden">
         <div class="w-full min-h-[100vh] pt-[25px] px-[75px] flex flex-col items-start justify-start">
             <div class="text-white text-[2.5vw] text-center w-full font-bold z-1">Services</div>
+            <div class="leaderboard-leaders" style="opacity: 1; transform: none;"><div class="LeaderboardPodium_scene3d__sG58k"><div class="leader leader-3d"><div class="leader__info"><div class="leader__info__1"><div class="" style="display: flex; gap: 1rem; align-items: center; justify-content: flex-start; flex-direction: column;"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="var(--clr-gold-crown)" color="var(--clr-gold-crown)" xmlns="http://www.w3.org/2000/svg" style="color: var(--clr-gold-crown); vertical-align: middle; margin-right: 0px; margin-left: 0px;"><path fill="currentColor" d="M4.075 15.407h15.891l1.05-4.235c1.225-.04 2.21-1.022 2.21-2.24 0-1.224-1.026-2.23-2.284-2.23-1.257 0-2.275 1.006-2.275 2.23 0 .293.05.56.15.812L16.5 11.351c-.198.137-.339.089-.447-.065L13.2 7.27a2.23 2.23 0 0 0 1.092-1.915c0-1.225-1.017-2.231-2.274-2.231-1.266 0-2.283 1.006-2.283 2.231a2.22 2.22 0 0 0 1.1 1.915l-2.83 3.983c-.107.146-.23.187-.43.057L5.21 9.671c.082-.235.124-.478.124-.738 0-1.225-1.018-2.231-2.275-2.231S.775 7.708.775 8.932c0 1.226 1 2.224 2.242 2.24l1.058 4.235Zm.39 1.566.314 1.241c.355 1.444 1.199 2.175 2.705 2.175h9.066c1.505 0 2.34-.723 2.705-2.175l.314-1.241H4.464Z"></path></svg><div class="leader-avatar  leader-avatar__1"><a class="avatar-link link hint--top" aria-label="@gabriele19" href="/player/gabriele19" style="pointer-events: auto;"><div style="position: relative;"><div class="avatar-link__image  avatar-link__image--pro"><img src="https://firebasestorage.googleapis.com/v0/b/cssbattleapp.appspot.com/o/user%2F5tYbW8kVmGO2qbk0u14n1XLFWSU2%2Favatar_5tYbW8kVmGO2qbk0u14n1XLFWSU2.png?alt=media" alt="gabriele19 avatar" class="leader__avatar" width="50" height="50"></div><div class="avatar-link__probadge-wrap"> <img height="15" width="30" class="pro-badge" src="" alt="Pro badge"></div></div></a></div><div class="" style="display: flex; gap: 0.3rem; align-items: center; justify-content: flex-start; flex-direction: column;"><div class="leader__name"><div style="display: inline-block; gap: 0.25rem; align-items: center;"><a class="name-link" href="/player/gabriele19" style="pointer-events: auto;"><span class=" ">Gabriele</span></a></div></div><p class="leader__score">162264.27 </p></div></div></div></div><div class="LeaderboardPodium_cuboid__qJQk7 LeaderboardPodium_cuboidLarge__Q8CJE" data-rank="1" data-index="1"><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_ft__jiOdc"><span class="LeaderboardPodium_rank__QfxoO">1</span></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_bk__hlkTr"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_rt__OQK5b"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_lt__tj4WS"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_bm__yNrza"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_tp__9IFLM"></div></div></div><div class="leader leader-3d"><div class="leader__info"><div class="leader__info__2"><div class="" style="display: flex; gap: 1rem; align-items: center; justify-content: flex-start; flex-direction: column;"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="var(--clr-silver-crown)" color="var(--clr-silver-crown)" xmlns="http://www.w3.org/2000/svg" style="color: var(--clr-silver-crown); vertical-align: middle; margin-right: 0px; margin-left: 0px;"><path fill="currentColor" d="M4.075 15.407h15.891l1.05-4.235c1.225-.04 2.21-1.022 2.21-2.24 0-1.224-1.026-2.23-2.284-2.23-1.257 0-2.275 1.006-2.275 2.23 0 .293.05.56.15.812L16.5 11.351c-.198.137-.339.089-.447-.065L13.2 7.27a2.23 2.23 0 0 0 1.092-1.915c0-1.225-1.017-2.231-2.274-2.231-1.266 0-2.283 1.006-2.283 2.231a2.22 2.22 0 0 0 1.1 1.915l-2.83 3.983c-.107.146-.23.187-.43.057L5.21 9.671c.082-.235.124-.478.124-.738 0-1.225-1.018-2.231-2.275-2.231S.775 7.708.775 8.932c0 1.226 1 2.224 2.242 2.24l1.058 4.235Zm.39 1.566.314 1.241c.355 1.444 1.199 2.175 2.705 2.175h9.066c1.505 0 2.34-.723 2.705-2.175l.314-1.241H4.464Z"></path></svg><div class="leader-avatar  leader-avatar__2"><a class="avatar-link link hint--top" aria-label="@beo" href="/player/beo" style="pointer-events: auto;"><div style="position: relative;"><div class="avatar-link__image  avatar-link__image--pro"><img src="https://firebasestorage.googleapis.com/v0/b/cssbattleapp.appspot.com/o/user%2F8wBrf63WLOOv8JuCeknfYk7t94B3%2Favatar_8wBrf63WLOOv8JuCeknfYk7t94B3.png?alt=media" alt="beo avatar" class="leader__avatar" width="50" height="50"></div><div class="avatar-link__probadge-wrap"> <img height="15" width="30" class="pro-badge" src="" alt="Pro badge"></div></div></a></div><div class="" style="display: flex; gap: 0.3rem; align-items: center; justify-content: flex-start; flex-direction: column;"><div class="leader__name"><div style="display: inline-block; gap: 0.25rem; align-items: center;"><a class="name-link" href="/player/beo" style="pointer-events: auto;"><span class=" ">Beo</span></a></div></div><p class="leader__score">162140.81 </p></div></div></div></div><div class="LeaderboardPodium_cuboid__qJQk7 " data-rank="2" data-index="2"><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_ft__jiOdc"><span class="LeaderboardPodium_rank__QfxoO">2</span></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_bk__hlkTr"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_rt__OQK5b"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_lt__tj4WS"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_bm__yNrza"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_tp__9IFLM"></div></div></div><div class="leader leader-3d"><div class="leader__info"><div class="leader__info__3"><div class="" style="display: flex; gap: 1rem; align-items: center; justify-content: flex-start; flex-direction: column;"><svg width="1em" height="1em" viewBox="0 0 24 24" fill="var(--clr-bronze-crown)" color="var(--clr-bronze-crown)" xmlns="http://www.w3.org/2000/svg" style="color: var(--clr-bronze-crown); vertical-align: middle; margin-right: 0px; margin-left: 0px;"><path fill="currentColor" d="M4.075 15.407h15.891l1.05-4.235c1.225-.04 2.21-1.022 2.21-2.24 0-1.224-1.026-2.23-2.284-2.23-1.257 0-2.275 1.006-2.275 2.23 0 .293.05.56.15.812L16.5 11.351c-.198.137-.339.089-.447-.065L13.2 7.27a2.23 2.23 0 0 0 1.092-1.915c0-1.225-1.017-2.231-2.274-2.231-1.266 0-2.283 1.006-2.283 2.231a2.22 2.22 0 0 0 1.1 1.915l-2.83 3.983c-.107.146-.23.187-.43.057L5.21 9.671c.082-.235.124-.478.124-.738 0-1.225-1.018-2.231-2.275-2.231S.775 7.708.775 8.932c0 1.226 1 2.224 2.242 2.24l1.058 4.235Zm.39 1.566.314 1.241c.355 1.444 1.199 2.175 2.705 2.175h9.066c1.505 0 2.34-.723 2.705-2.175l.314-1.241H4.464Z"></path></svg><div class="leader-avatar  leader-avatar__3"><a class="avatar-link link hint--top" aria-label="@jeme" href="/player/jeme" style="pointer-events: auto;"><div style="position: relative;"><div class="avatar-link__image  avatar-link__image--pro"><img src="https://firebasestorage.googleapis.com/v0/b/cssbattleapp.appspot.com/o/user%2FKsN3Eju3tSStMIaVUnPa3ZZBZ4J2%2Favatar_KsN3Eju3tSStMIaVUnPa3ZZBZ4J2.jpeg?alt=media" alt="jeme avatar" class="leader__avatar" width="50" height="50"></div><div class="avatar-link__probadge-wrap"> <img height="15" width="30" class="pro-badge" src="" alt="Pro badge"></div></div></a></div><div class="" style="display: flex; gap: 0.3rem; align-items: center; justify-content: flex-start; flex-direction: column;"><div class="leader__name"><div style="display: inline-block; gap: 0.25rem; align-items: center;"><a class="name-link" href="/player/jeme" style="pointer-events: auto;"><span class=" ">Jeroen Meijers</span></a></div></div><p class="leader__score">162128.36 </p></div></div></div></div><div class="LeaderboardPodium_cuboid__qJQk7 " data-rank="3" data-index="3"><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_ft__jiOdc"><span class="LeaderboardPodium_rank__QfxoO">3</span></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_bk__hlkTr"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_rt__OQK5b"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_lt__tj4WS"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_bm__yNrza"></div><div class="LeaderboardPodium_face__oto6T LeaderboardPodium_tp__9IFLM"></div></div></div></div></div>
         </div>
         <div class="h-[200px] w-full bg-[linear-gradient(180deg,#000000,rgba(0,0,0,0))] absolute top-0 z-0"></div>
         <div class="h-[100px] w-full bg-[linear-gradient(0deg,#242424,rgba(0,0,0,0))] absolute bottom-0 z-0"></div>
@@ -13,5 +14,281 @@
 </template>
 
 <style scoped>
-
+.leader__info {
+    position: absolute;
+    bottom: calc(100% + .5rem);
+    width: max-content;
+    transform: translateZ(1.5rem);
+}
+.LeaderboardPodium_cuboid__qJQk7[data-rank="1"] {
+    transform: rotateX(var(--rx)) translateY(24px) translateZ(var(--tz));
+}
+.LeaderboardPodium_scene3d__sG58k div {
+    transform-style: preserve-3d;
+}
+.LeaderboardPodium_cuboid__qJQk7 {
+    transition: transform .125s ease;
+}
+.LeaderboardPodium_cuboidLarge__Q8CJE {
+    --w: clamp(0px, 24vw, 20rem);
+    --h: 220px;
+}
+.LeaderboardPodium_cuboid__qJQk7 {
+    --w: clamp(0px, 22vw, 17rem);
+    --b: 12em;
+    --h: 9em;
+    --rx: -40deg;
+    --ry: 5deg;
+    --clr-border-glow: hsla(0, 0%, 100%, .2);
+    --tz: 0;
+    position: absolute;
+    display: inline-flex;
+    position: relative;
+    width: var(--w);
+    height: var(--h);
+}
+.LeaderboardPodium_scene3d__sG58k div {
+    transform-style: preserve-3d;
+}
+.leader [data-rank="1"] span {
+    font-size: 11rem;
+}
+.LeaderboardPodium_rank__QfxoO {
+    font-size: 8rem;
+    color: var(--podium-text-color);
+    font-family: ClashGrotesk;
+    font-weight: 700;
+    transition: .2s ease;
+}
+* {
+    margin: 0;
+    box-sizing: border-box;
+}
+.LeaderboardPodium_ft__jiOdc:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: radial-gradient(167.5% 203.72% at 48.5% -21.62%, rgb(var(--podium-rgb-1)) 0, rgba(var(--podium-rgb-0), .5) 100%);
+    opacity: .6;
+    z-index: -1;
+}
+.LeaderboardPodium_cuboidLarge__Q8CJE {
+    --w: clamp(0px, 24vw, 20rem);
+    --h: 220px;
+}
+.LeaderboardPodium_bk__hlkTr, .LeaderboardPodium_ft__jiOdc {
+    width: 100%;
+    height: 100%;
+}
+.LeaderboardPodium_ft__jiOdc {
+    border-width: 1.5px;
+    border-style: solid;
+    border-image: linear-gradient(to bottom, var(--clr-border-glow), hsla(0, 0%, 100%, 0)) 1;
+    background: linear-gradient(180deg, rgb(var(--podium-rgb-0)) 0, rgba(var(--podium-rgb-0), 0) 100%);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+}
+.LeaderboardPodium_face__oto6T {
+    position: absolute;
+    overflow: hidden;
+    transform-origin: 0 0;
+    backface-visibility: hidden;
+}
+.LeaderboardPodium_scene3d__sG58k {
+    display: flex;
+    gap: .25rem;
+    align-items: flex-end;
+    justify-content: center;
+    perspective: 600px;
+    width: 100%;
+    padding: 12rem 5rem 2rem;
+    background: var(--clr-bg);
+    font-size: 100%;
+    transition: .5s ease;
+    font-family: ClashGrotesk;
+    font-weight: 500;
+    position: relative;
+}
+.leader:has([data-rank="1"]) {
+    z-index: 1;
+}
+.LeaderboardPodium_scene3d__sG58k div {
+    transform-style: preserve-3d;
+}
+.leader {
+    position: relative;
+    align-items: center;
+}
+.leader, .leaderboard__user-list {
+    display: flex
+;
+    flex-direction: column;
+}
+* {
+    margin: 0;
+    box-sizing: border-box;
+}
+div {
+    display: block;
+    unicode-bidi: isolate;
+}
+.LeaderboardPodium_scene3d__sG58k {
+    display: flex;
+    gap: .25rem;
+    align-items: flex-end;
+    justify-content: center;
+    perspective: 600px;
+    width: 100%;
+    padding: 12rem 5rem 2rem;
+    background: var(--clr-bg);
+    font-size: 100%;
+    transition: .5s ease;
+    font-family: ClashGrotesk;
+    font-weight: 500;
+    position: relative;
+}
+body, html {
+    height: 100%;
+    color: var(--clr-text);
+    background: var(--clr-bg);
+    font-family: IBM Plex Mono, Menlo, DejaVu Sans Mono, Lucida Console, monospace;
+    -webkit-font-smoothing: antialiased;
+    scrollbar-gutter: stable both-edges;
+}
+:root {
+    --command-palette-animation: fadein 0.35s;
+}
+:root {
+    --btn-size: 158px;
+    --offset: calc(var(--btn-size) / 10);
+}
+:root {
+    color-scheme: dark;
+}
+:root {
+    --font-size-0: 0.875rem;
+    --font-size-1: 1rem;
+    --font-size-2: 1.125rem;
+    --font-size-3: 1.375rem;
+    --font-size-3-5: 1.5rem;
+    --font-size-3-75: 1.75rem;
+    --font-size-4: 2rem;
+    --font-size-5: 3rem;
+    --font-size-6: 4rem;
+    --clr-bg: #07080b;
+    --clr-sidebar-bg: #171d23;
+    --clr-scrollbar: hsla(0, 0%, 100%, .2);
+    --clr-highlight: hsla(0, 0%, 100%, .1);
+    --clr-header-bg: #0f1117;
+    --clr-footer-bg: #0f1117;
+    --clr-banner-bg: #0f1117;
+    --clr-centered-box-bg: #0f1117;
+    --clr-inner-header: #21272d;
+    --clr-inner-header-highlight: #2a313a;
+    --clr-tile-header: #212a32;
+    --clr-box-bg: #13181c;
+    --clr-box-bg-darker: #121518;
+    --clr-editor-bg: #07080b;
+    --clr-colorlist: #000;
+    --clr-border: #27313a;
+    --clr-border-brighter: #323f4a;
+    --clr-target-tile-bg: #27313a;
+    --clr-target-tile-bg-lighter: #323e48;
+    --clr-active-filter: #27313a;
+    --clr-dropdown-bg: #27313a;
+    --clr-dropdown-btn: #43515e;
+    --clr-btn: #323f4a;
+    --clr-btn-hover: #43515e;
+    --clr-btn-primary: #0060ca;
+    --clr-btn-primary-hover: #0050b0;
+    --clr-btn-danger: #923c3c;
+    --clr-btn-danger-hover: #7f2f2f;
+    --clr-btn-success: #1e5222;
+    --clr-btn-success-hover: #16421a;
+    --clr-btn-text: #fff;
+    --clr-btn-secondary-text: #fff;
+    --clr-input: #0f1117;
+    --clr-brand: #ffdf00;
+    --clr-loader: #fff;
+    --clr-link: #fff;
+    --clr-heading: #eff5fb;
+    --clr-text: #cbd1e1;
+    --clr-tab-bg-selected: #323f4a;
+    --clr-header-text: #cbd1e1;
+    --clr-text-light: #a0b3c6;
+    --toastify-color-light: #fff;
+    --toastify-color-dark: #121212;
+    --toastify-color-info: #3498db;
+    --toastify-color-success: #07bc0c;
+    --toastify-color-warning: #f1c40f;
+    --toastify-color-error: #e74c3c;
+    --toastify-color-transparent: hsla(0, 0%, 100%, .7);
+    --toastify-icon-color-info: var(--toastify-color-info);
+    --toastify-icon-color-success: var(--toastify-color-success);
+    --toastify-icon-color-warning: var(--toastify-color-warning);
+    --toastify-icon-color-error: var(--toastify-color-error);
+    --toastify-toast-width: 320px;
+    --toastify-toast-background: #fff;
+    --toastify-toast-min-height: 64px;
+    --toastify-toast-max-height: 800px;
+    --toastify-font-family: sans-serif;
+    --toastify-z-index: 9999;
+    --toastify-text-color-light: #757575;
+    --toastify-text-color-dark: #fff;
+    --toastify-text-color-info: #fff;
+    --toastify-text-color-success: #fff;
+    --toastify-text-color-warning: #fff;
+    --toastify-text-color-error: #fff;
+    --toastify-spinner-color: #616161;
+    --toastify-spinner-color-empty-area: #e0e0e0;
+    --toastify-color-progress-light: linear-gradient(90deg, #4cd964, #5ac8fa, #007aff, #34aadc, #5856d6, #ff2d55);
+    --toastify-color-progress-dark: #bb86fc;
+    --toastify-color-progress-info: var(--toastify-color-info);
+    --toastify-color-progress-success: var(--toastify-color-success);
+    --toastify-color-progress-warning: var(--toastify-color-warning);
+    --toastify-color-progress-error: var(--toastify-color-error);
+}
+:root {
+    --reach-dialog: 1;
+}
+@media screen and (max-width: 600px) {
+    html {
+        font-size: .8em;
+    }
+}
+html {
+    font-size: 1em;
+}
+@media (max-width: 600px) {
+    ::-webkit-scrollbar {
+        width: 0 !important;
+        height: 0 !important;
+    }
+}
+@media (max-width: 600px) {
+    ::-webkit-scrollbar-thumb {
+        background: transparent;
+        border-radius: 0;
+    }
+}
+@media (max-width: 600px) {
+    ::-webkit-scrollbar-track {
+        background: transparent;
+    }
+}
+--border-x {
+    syntax: "<length>";
+    inherits: false;
+    initial-value: 0;
+}
+--border-rotation {
+    syntax: "<angle>";
+    inherits: false;
+    initial-value: 0deg;
+}
 </style>
